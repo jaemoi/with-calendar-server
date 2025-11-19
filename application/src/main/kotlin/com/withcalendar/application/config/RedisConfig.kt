b8bac24f4bf5da17014e2f324a27f25b26991d79
jaemoi <jaemoi@naver.com>
2025-11-19 22:51:48 +0900

    1. validation 의존성 추가 2. application.yml 프로필 분리

--- application/src/main/kotlin/com/withcalendar/application/config/RedisConfig.kt
+++ application/src/main/kotlin/com/withcalendar/application/config/RedisConfig.kt
@@ -1,5 +1,6 @@
 package com.withcalendar.application.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -7,11 +8,14 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.StringRedisTemplate
 
 @Configuration
-class RedisConfig {
+class RedisConfig(
+    @Value("\${redis.host}") private val host: String,
+    @Value("\${redis.port}") private val port: Int
+) {
 
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory("localhost", 6379)
+        return LettuceConnectionFactory(host, port)
     }
 
     @Bean
